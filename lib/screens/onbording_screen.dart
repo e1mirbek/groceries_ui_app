72dd4c4c40ab0459bc34b9bf392ac3630d59a24d
e1mirbek <elmirbekabdymanapov121@gmail.com>
2025-11-07 21:59:55 +0600

    feat: добавить шаблон экрана онбординга с верхним AppBar

--- lib/screens/onbording_screen.dart
+++ lib/screens/onbording_screen.dart
@@ -10,10 +10,36 @@ class OnbordingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Center(
-        child: Text("OnbordingScreen Page"),
-      ),
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(0xFFFFFFFF),
+          appBar: AppBar(
+            backgroundColor: Color(0xFFFFFFFF),
+            centerTitle: true,
+            title: Padding(
+              padding: const EdgeInsets.only(top: 58.29),
+              child: Column(
+                children: [
+                  Image.asset("assets/images/Group.png",),
+                  const SizedBox(height: 5,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("assets/icons/Exclude.png"),
+                      const SizedBox(width: 7.44,),
+                      Text("Dhaka, Banassre", style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18.0, color: Color(0xFF4C4F4D)),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            toolbarHeight: 140,
+          ),
+      body:  Column(
+        children: [
+
+        ],
+      )
     ));
   }
 }
